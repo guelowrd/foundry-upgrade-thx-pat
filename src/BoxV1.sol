dbafe903a0234d7dade1155887b5bddab3c1a8e4
guelowrd <gaylord.warner@gmail.com>
2023-08-01 12:02:15 +0100

    use OZ upgradeable contract

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -2,7 +2,9 @@
 
 pragma solidity ^0.8.18;
 
-contract BoxV1 {
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract BoxV1 is UUPSUpgradeable {
     uint256 internal number;
 
     function getNumber() external view returns (uint256) {
@@ -12,4 +14,6 @@ contract BoxV1 {
     function version() external pure returns (uint256) {
         return 1;
     }
+
+    function _authorizeUpgrade(address newImplementation) internal override {}
 }
