ba4b715897d3e3e34dcab3df64113d51847f3c6b
guelowrd <gaylord.warner@gmail.com>
2023-08-01 12:07:42 +0100

    make proy Initializable

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -3,9 +3,15 @@
 pragma solidity ^0.8.18;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract BoxV1 is UUPSUpgradeable {
+contract BoxV1 is UUPSUpgradeable, Initializable {
     uint256 internal number;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
 
     function getNumber() external view returns (uint256) {
         return number;
